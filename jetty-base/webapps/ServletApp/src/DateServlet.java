d51d21a93544c3037926e05ae216d3d0df23657d
Carlos <carlossz.206@gmail.com>
2021-06-08 19:17:42 -0700

    multiple servlets & static with dynamic pages

--- jetty-base/webapps/ServletApp/src/DateServlet.java
+++ jetty-base/webapps/ServletApp/src/DateServlet.java
@@ -1,19 +1,18 @@
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Date;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class HelloWorldServlet extends HttpServlet {
+public class DateServlet extends HttpServlet {
 
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		
-        String requestUrl = request.getRequestURI();
-		String name = requestUrl.substring("/ServletApp/hello/".length());
+
+		Date now = new Date();
 
 		PrintWriter out = response.getWriter();
-		out.println("<h1>Hello " + name + "</h1>");
-		out.println("<p>Nice to meet you!</p>");
+		out.println("<p>Current time: " + now.toString() + "</p>");
 	}
 }
